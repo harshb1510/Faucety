3a613fd85012679fb4316933f92ba863cf9c120a
Akshat Gupta <akshatgtc@gmail.com>
2024-03-12 21:46:17 +0530

    Stardeum

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,14 +11,11 @@ import Home from "./Pages/Home.jsx";
 import Invest from "./Pages/Invest.jsx";
 import Scan from "./Pages/Scan.jsx";
 import DepositByINR from "./Pages/DepositByINR.jsx";
-<<<<<<< HEAD
 import Borrow from "./Pages/Borrow.jsx";
-=======
 import Profile from "./Pages/Profile.jsx";
 
 
 
->>>>>>> 640f5c32c052022148adb79451f5190096e99bc1
 
 const App = () => (
   <div className="App">
@@ -27,15 +24,6 @@ const App = () => (
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-<<<<<<< HEAD
-        <Route path="/deposit" element={<Deposit />} />
-        <Route path="/depositByINR" element={<DepositByINR />} />
-        <Route path="/scan" element={<Scan />} />
-        <Route path="/nftMint" element={<NFTMint />} />
-        <Route path="/claimNft" element={<ClaimNft />} />
-        <Route path="/invest" element={<Invest />} />
-        <Route path="/borrow" element={<Borrow />} />
-=======
         <Route path ='/deposit' element={<Deposit />} />
         <Route path= "/depositByINR" element={<DepositByINR/>}/>
         <Route path ='/scan' element={<Scan />} />
@@ -43,7 +31,7 @@ const App = () => (
         <Route path="/claimNft" element={<ClaimNft/>} />
         <Route path='/invest'element={<Invest/>}/>
         <Route path='/profile' element={<Profile/>}/>
->>>>>>> 640f5c32c052022148adb79451f5190096e99bc1
+        <Route path="/borrow" element={<Borrow />} />
       </Routes>
       <ToastContainer />
     </Router>
